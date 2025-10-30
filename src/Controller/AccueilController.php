2e14a053f8ac2c8b9effa67d58e6626cf0f5a82b
Cosmiaou <160436481+Cosmiaou@users.noreply.github.com>
2025-10-30 15:12:58 +0100

    Phase 3 - twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,12 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @author louis
  */
-class AccueilController {
+class AccueilController extends AbstractController {
     
     
     
     #[Route('/', name: 'accueil')]
     public function index() : Response {
-        return new Response('Hello world !');
+        return $this->render("pages/accueil.html.twig");
     }
 }
